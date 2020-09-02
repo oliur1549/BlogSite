7052e853028d18bb444e698b06933258bacbd6d5
oliur <oliur1549@gmail.com>
2020-09-02 20:40:35 +0600

    1.1 migration seed work complete

--- BlogSite.Web/Controllers/AccountsController.cs
+++ BlogSite.Web/Controllers/AccountsController.cs
@@ -73,7 +73,7 @@ namespace BlogSite.Web.Controllers
                 var user = await _userManager.FindByNameAsync(model.UserName);
                 var roles = await _userManager.GetRolesAsync(user);
 
-                if(roles[0] == "Customer")
+                if (roles[0] == "Customer")
                 {
                     return LocalRedirect("~/User/Dashboard/Index");
                 }
@@ -81,7 +81,6 @@ namespace BlogSite.Web.Controllers
                 {
                     return LocalRedirect("~/Admin/Dashboard/Index");
                 }
-                return LocalRedirect(model.ReturnUrl);
             }
             if (result.RequiresTwoFactor)
             {
@@ -97,67 +96,9 @@ namespace BlogSite.Web.Controllers
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return View(model);
             }
-
+            
         }
 
-        //public IActionResult Register(string returnUrl)
-        //{
-        //    var model = new RegisterModel();
-
-        //    ViewBag.ReturnUrl = returnUrl;
-        //    return View(model);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Register(RegisterModel model, string returnUrl = null)
-        //{
-        //    returnUrl = returnUrl ?? Url.Content("~/");
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = new ApplicationUser
-        //        {
-        //            UserName = model.UserName,
-        //            FullName = model.FullName,
-        //            Email = model.Email
-        //        };
-        //        var result = await _userManager.CreateAsync(user, model.Password);
-
-        //        if (result.Succeeded)
-        //        {
-        //            _registerLogger.LogInformation("User created a new account with password.");
-        //            await _userManager.AddToRoleAsync(user, "Customer");
-
-        //            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-        //            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-        //            var callbackUrl = Url.Page(
-        //                "/Account/ConfirmEmail",
-        //                pageHandler: null,
-        //                values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-        //                protocol: Request.Scheme);
-
-        //            //await _emailSender.SendEmailAsync(model.Email, "Confirm your email",
-        //            //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-        //            if (_userManager.Options.SignIn.RequireConfirmedAccount)
-        //            {
-        //                //return RedirectToPage("RegisterConfirmation", new { email = model.Email, returnUrl = returnUrl });
-        //            }
-        //            else
-        //            {
-        //                await _signInManager.SignInAsync(user, isPersistent: false);
-        //                return LocalRedirect("~/User/Dashboard/Index");
-        //                //return LocalRedirect(returnUrl);
-        //            }
-        //        }
-
-        //        foreach (var error in result.Errors)
-        //        {
-        //            ModelState.AddModelError(string.Empty, error.Description);
-        //        }
-        //    }
-        //    return View(model);
-        //}
 
     }
 }
